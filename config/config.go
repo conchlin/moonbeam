e16fa2f9a926c53990c546874670626f1612a4b6
conchlin <connorbeckett@protonmail.com>
2024-03-06 18:36:43 -0500

    replace toml with json begin fleshing out how to add guild member data to json file

--- config/config.go
+++ config/config.go
@@ -1,27 +1,53 @@
 package config
 
 import (
+	"encoding/json"
 	"log"
-
-	"github.com/BurntSushi/toml"
+	"os"
 )
 
-// discord specific info
-type discordConfig struct {
-	SecurityToken string
+// player specific info
+type MemberInfo struct {
+	Guild  string `json:"guild"`
+	Name   string `json:"name"`
+	Level  int    `json:"level"`
+	Exp    string `json:"exp"`
+	Gender string `json:"gender"`
+	Job    string `json:"job"`
+	Quests int    `json:"quests"`
+	Cards  int    `json:"cards"`
+	Donor  bool   `json:"donor"`
+	Fame   int    `json:"fame"`
 }
 
 // overarching categories
 type Config struct {
-	Discord discordConfig
+	Discord discordConfig `json:"discord"`
+	Guild   guildConfig   `json:"guild"`
+}
+
+// discord specific info
+type discordConfig struct {
+	SecurityToken string `json:"securityToken"`
+}
+
+// guild member specific info
+type guildConfig struct {
+	Members []MemberInfo `json:"members"`
 }
 
 func ParseConfig() *Config {
-	tomlFile := &Config{}
+	jsonFile, err := os.Open("config/config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
 
-	if _, err := toml.DecodeFile("config/config.toml", tomlFile); err != nil {
+	jsonDecoder := json.NewDecoder(jsonFile)
+	config := &Config{}
+	if err := jsonDecoder.Decode(config); err != nil {
 		log.Fatal(err)
 	}
 
-	return tomlFile
+	return config
 }
