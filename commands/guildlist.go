17700819714801464991ffb06eb7a9642b0b2cc1
conchlin <connorbeckett@protonmail.com>
2024-08-21 22:28:13 -0400

    guild list command

---
+++ commands/guildlist.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"fmt"
+	"moonbeam/config"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"golang.org/x/text/collate"
+	"golang.org/x/text/language"
+)
+
+var currentMemberList []string
+
+func HandleGuildList(session *discordgo.Session, message *discordgo.MessageCreate) {
+	var formattedList strings.Builder
+	loadCurrentGuildMembers()
+
+	c := collate.New(language.English, collate.IgnoreCase)
+	c.SortStrings(currentMemberList)
+	for _, char := range currentMemberList {
+		formattedList.WriteString(fmt.Sprintf(char + "\r\n"))
+	}
+
+	session.ChannelMessageSendComplex(message.ChannelID, &discordgo.MessageSend{
+		Embed: &discordgo.MessageEmbed{
+			Title:       "Guild List",
+			Description: formattedList.String(),
+			Color:       0x2cdaca,
+		},
+	})
+}
+
+func loadCurrentGuildMembers() {
+	cfg := config.ParseConfig()
+
+	for _, member := range cfg.Guild.Members {
+		currentMemberList = append(currentMemberList, member.Name)
+	}
+
+}
