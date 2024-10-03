15071fd5101905ff3ff08d5b238d21b861a71740
conchlin <connorbeckett@protonmail.com>
2024-10-03 10:20:17 -0400

    guildlist now uses Player struct instead of []string

--- commands/guildlist.go
+++ commands/guildlist.go
@@ -3,41 +3,46 @@ package commands
 import (
 	"fmt"
 	"moonbeam/config"
+	"moonbeam/utils"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"golang.org/x/text/collate"
-	"golang.org/x/text/language"
 )
 
-var currentMemberList []string
+var currentMemberList []utils.Player
 
 func HandleGuildList(session *discordgo.Session, message *discordgo.MessageCreate) {
 	var formattedList strings.Builder
-	loadCurrentGuildMembers()
 
-	c := collate.New(language.English, collate.IgnoreCase)
-	c.SortStrings(currentMemberList)
+	go func() {
+		loadCurrentGuildMembers()
 
-	formattedList.WriteString(fmt.Sprintf("Total Members: %v\n\n", len(currentMemberList)))
-	for _, char := range currentMemberList {
-		formattedList.WriteString(fmt.Sprintf(char + "\r\n"))
-	}
+		formattedList.WriteString(fmt.Sprintf("Total Members: %v\n\n", len(currentMemberList)))
+		formattedList.WriteString("```")
+		for _, char := range currentMemberList {
+			formattedList.WriteString(fmt.Sprintf("%v\t Lv%v\t %v\r\n", char.Name, char.Level, char.Job))
+		}
+		formattedList.WriteString("```")
 
-	session.ChannelMessageSendComplex(message.ChannelID, &discordgo.MessageSend{
-		Embed: &discordgo.MessageEmbed{
-			Title:       "Guild List",
-			Description: formattedList.String(),
-			Color:       0x2cdaca,
-		},
-	})
+		session.ChannelMessageSendComplex(message.ChannelID, &discordgo.MessageSend{
+			Embed: &discordgo.MessageEmbed{
+				Title:       "Guild List",
+				Description: formattedList.String(),
+				Color:       0x2cdaca,
+			},
+		})
+	}()
+
+	//c := collate.New(language.English, collate.IgnoreCase)
+	//c.SortStrings(currentMemberList)
 }
 
 func loadCurrentGuildMembers() {
 	cfg := config.ParseConfig()
 
 	for _, member := range cfg.Guild.Members {
-		currentMemberList = append(currentMemberList, member.Name)
+		player := config.ConvertJsonToPlayer(member)
+		currentMemberList = append(currentMemberList, player)
 	}
 
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,4 @@ require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.17.0
 )
